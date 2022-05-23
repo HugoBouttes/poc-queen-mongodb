70891a02f13177ac302b872013611a9974f7e4d4
HBouttes <103732523+BouttesINSEE@users.noreply.github.com>
2022-05-23 09:39:50 +0200

    update apps/demoqueenmongo

--- apps/DemoQueenMongo/src/main/java/insee/stage/demo/model/InseeContext.java
+++
@@ -1,6 +0,0 @@
-package insee.stage.demo.model;
-
-public enum InseeContext {
-    HOUSEHOLD , DEFAULT , BUSINESS
-
-}
--- apps/DemoQueenMongo/src/main/java/insee/stage/demo/model/State.java
+++
@@ -1,6 +0,0 @@
-package insee.stage.demo.model;
-
-public enum State {
-    NOT_STARTED, STARTED, VALIDATED
-
-}
