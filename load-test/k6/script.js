efac31af2258ed3960a61c564698dd71f7b27ced
HBouttes <103732523+BouttesINSEE@users.noreply.github.com>
2022-07-06 09:12:38 +0200

    échec script 5

--- load-test/k6/script.js
+++ load-test/k6/script.js
@@ -6,11 +6,11 @@ import { check, sleep, group } from 'k6';
 
 export const options = {
   stages: [
-    { duration: "20m", target: 1 }, // simulate ramp-up of traffic from 0 to 1 users over 20 minutes.
-    { duration: "60m", target: 1 }, // stay at 1 users for 60m minutes
+    { duration: "20m", target: 334 }, // simulate ramp-up of traffic from 0 to 334 users over 20 minutes.
+    { duration: "60m", target: 334 }, // stay at 334 users for 60m minutes
     { duration: "20m", target: 0 }, // ramp-down to 0 users over 20 minutes
   ],
-  //vus: 1,
+  //vus: 334,
   //iterations: 1,
   //duration: "3600s",
   setupTimeout: "300s",
