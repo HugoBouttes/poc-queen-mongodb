af601d7ef89695ca1063a54c3c35d3f27894fa07
HBouttes <103732523+BouttesINSEE@users.noreply.github.com>
2022-08-05 10:36:08 +0200

    650

--- load-test/k6/script.js
+++ load-test/k6/script.js
@@ -6,11 +6,11 @@ import { check, sleep, group } from 'k6';
 
 export const options = {
   stages: [
-  { duration: "20m", target: 1300 }, // simulate ramp-up of traffic from 0 to 1300 users over 20 minutes.
-  { duration: "60m", target: 1300 }, // stay at 1300 users for 60m minutes
+  { duration: "20m", target: 650 }, // simulate ramp-up of traffic from 0 to 650 users over 20 minutes.
+  { duration: "60m", target: 650 }, // stay at 650 users for 60m minutes
   { duration: "20m", target: 0 }, // ramp-down to 0 users over 20 minutes
   ],
-  //vus: 1300,
+  //vus: 650,
   //iterations: 1,
   //duration: "3600s",
   setupTimeout: "300s",
